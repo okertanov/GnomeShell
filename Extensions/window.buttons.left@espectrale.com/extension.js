f76d1c43d764569619ebedc7988c19f0e8dad2d0
okertanov <okertanov@gmail.com>
2012-11-14 12:01:06 +0200

    new .gitignore, more comments

--- Extensions/window.buttons.left@espectrale.com/extension.js
+++ Extensions/window.buttons.left@espectrale.com/extension.js
@@ -1,14 +1,37 @@
 /*
     Copyright (C) 2012 Oleg Kertanov <okertanov@gmail.com>
-    Created with `gnome-shell-extension-tool --create-extension`
+    Created with
+        `gnome-shell-extension-tool --create-extension`
 
     See also:
+        * dconf-editor
         * /usr/share/gnome-shell/js
+
+    Links:
+        * [live.gnome.org Extensions](https://live.gnome.org/GnomeShell/Extensions)
+        * [live.gnome.org Getting Started development](https://live.gnome.org/GnomeShell/Development)
+        * [Updating GNOME Shell Extensions To Work With GNOME 3.2](http://blog.fpmurphy.com/2011/11/updating-gnome-shell-extensions-to-work-with-gnome-3-2.html)
+        * [GIO Reference Manual](http://developer.gnome.org/gio/stable/)
+        * [St Reference Manual](http://developer.gnome.org/st/stable/)
+        * [Clutter Reference Manual](http://developer.gnome.org/clutter/1.12/)
+        * [Tweener Documentation](http://hosted.zeh.com.br/tweener/docs/en-us/)
 */
 
+/*
+    Modules
+*/
 const Lang  =   imports.lang;
-const St    =   imports.gi.St;
 const Main  =   imports.ui.main;
+const St    =   imports.gi.St;
+const Gio   =   imports.gi.Gio;
+
+/*
+    Constants
+*/
+const CFG_BUTTON_LAYOUT_PATH = 'org.gnome.shell.overrides';
+const CFG_BUTTON_LAYOUT_KEY  = 'button-layout';
+const CFG_BUTTON_LAYOUT_VAL_LTR_FULL = 'close,minimize,maximize:';
+const CFG_BUTTON_LAYOUT_VAL_RTL_FULL = ':minimize,maximize,close';
 
 var WindowButtons = function()
 {
